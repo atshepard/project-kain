b01a7d3fb045380a277511ee3d65bf116488516e
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-04-22 16:14:50 -0500

    pulls friends and user reducer information for display on DOM in FriendList component

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,13 +1,32 @@
+import {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+
 function FriendList () {
+    const dispatch = useDispatch();
+
+    const people = useSelector((store) => store.allUsersReducer);
+    const friends = useSelector((store) => store.friendReducer);
 
     useEffect(() => {
         dispatch({ type: 'FETCH_FUTURE_FRIENDS'});
+        dispatch({ type: 'FETCH_MY_FRIENDS'});
     }, []);
 
 
  return(<>
- 
- 
+    <p>All people:</p>
+    <ul>
+        {people.map((people, i) => {
+            return(<li key={i}>{people.display_name}</li>)
+        })}
+    </ul>
+    <br />
+    <p>My people:</p>
+    <ul>
+        {friends.map((friend, i) => {
+            return(<li key={i}>{friend.display_name}</li>)
+        })}
+    </ul>
  </>)   
 }
 
