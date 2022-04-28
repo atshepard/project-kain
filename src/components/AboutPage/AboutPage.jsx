ca21f6ce27ad56083f0c142d710fad50e38fb8f2
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-04-28 14:19:06 -0500

    adds cards for friends list

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -30,13 +30,10 @@ function AboutPage() {
         <h3>Project KAIN was built using a wide array of technologies: </h3>
         <img src={`/images/used.png`}></img>
         <br />
-        <p>Deployment: </p>
-        <ul>
-          <li>Heroku</li>
-        </ul>
         <p>Styling: </p>
         <ul>
           <li>Material UI</li>
+          <li>CSS</li>
         </ul>
         <p>Functionality: </p>
         <ul>
@@ -44,10 +41,12 @@ function AboutPage() {
           <li>React</li>
           <li>Redux</li>
           <li>Redux-Saga</li>
-          <li>HTML5</li>
           <li>NodeJS</li>
           <li>ExpressJS</li>
           <li>PostgreSQL</li>
+          <li>Google Maps Javascript API</li>
+          <li>Google Places API</li>
+          <li>Google Geolocation API</li>
         </ul>
       </div>
     </div>
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,48 +1,68 @@
-import {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {TextField, Stack, Autocomplete, Avatar} from '@mui/material';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+import { Box, TextField, Stack, Autocomplete, Avatar, Card, CardHeader, Button, CardContent } from '@mui/material';
 
 
-function FriendList () {
+function FriendList() {
     const dispatch = useDispatch();
 
     const people = useSelector((store) => store.allUsersReducer);
     const friends = useSelector((store) => store.friendReducer);
+    const [value, setValue] = useState('');
 
     useEffect(() => {
-        dispatch({ type: 'FETCH_FUTURE_FRIENDS'});
-        dispatch({ type: 'FETCH_MY_FRIENDS'});
+        dispatch({ type: 'FETCH_FUTURE_FRIENDS' });
+        dispatch({ type: 'FETCH_MY_FRIENDS' });
     }, []);
 
-
- return(<>
-    <p>Find a Friend:</p>
-    <div className="container">
-    <Stack spacing={2} sx={{ width: 300 }}>
-      <Autocomplete
-        id="people list"
-        freeSolo
-        options={people.map((person) => person.display_name)}
-        renderInput={(params) => <TextField {...params} label="Add a Friend" />}
-      />
-    </Stack>
-    </div>
-    
-    <br />
-    <p>Your contacts:</p>
-    
-    {friends && 
-        <Stack direction="row" spacing={2}>
-        {friends.map((friend, i) => {
-                return(
-                    <Avatar key={i} alt={friend.display_name} src={friend.profile_image} />
-                )
-            })}
-        </Stack>
+    const addFriend = () => {
+        console.log(value);
+        //axios.post(`/api/friend`, {})
     }
+
+
+    return (<>
+        <p>Find a Friend:</p>
+        <div className="container">
+            <Stack spacing={2} sx={{ width: '50vw' }}>
+                <Autocomplete
+                    id="people list"
+                    freeSolo
+                    inputValue={value}
+                    onInputChange={(event, newInputValue) => {
+                      setValue(newInputValue);
+                    }}
+                    options={people.map((person) => person.display_name)}
+                    renderInput={(params) => <TextField {...params} label="Add a Friend" />}
+                />
+            </Stack>
+            <Button onClick={addFriend}>ADD FRIEND</Button>
+        </div>
+
+        <br />
+        <p>Your Friends:</p>
+        {friends &&
+            // <Stack direction="row" spacing={2}>
+                friends.map((friend, i) => {
+                    return (
+                        <div key={i} className="cardContainer">
+                            <Box display="flex" alignItems="center" justifyContent="center">
+                            <Card sx={{width: '20vw', m: 2, p: 1}}>
+                                <CardHeader align="center" title={friend.display_name}/>
+                                <CardContent align="center">
+                                    <Avatar sx={{ width: 80, height: 80}} src={friend.profile_image} />
+                                </CardContent>
+                            </Card>
+                            </Box>
+                        </div>
+                    )
+                })}
+            {/* </Stack> */}
         
 
- </>)   
+
+    </>)
 }
 
 export default FriendList;
